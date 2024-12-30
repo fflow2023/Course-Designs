ccb21d9be7ab3a9beb3c87ff0a63c537868185ca
LYQ <148413354+fflow2023@users.noreply.github.com>
2024-12-30 10:12:42 +0800

    Delete 仅供参考 directory

--- 仅供参考/课程管理系统/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        CourseManager manager = new CourseManager();
-        CourseManagementGUI gui = new CourseManagementGUI(manager);
-        gui.setVisible(true);
-    }
-}
